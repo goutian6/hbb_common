f87621e6bc7384a0cc9faad4f7214656a39a5e99
goutian6 <3318402274@qq.com>
2026-03-19 12:56:34 +0800

    改为自己的ID/中继服务器

--- src/config.rs
+++ src/config.rs
@@ -106,8 +106,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["10.10.66.125"];
+pub const RS_PUB_KEY: &str = "xoy50ROVbb8QX28BKs1kGDwHggkn2ScxDkb25aOf+kQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
